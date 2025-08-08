6fdf2fd0f867f86b113a17dbe9afab33130cb6ff
Amol <babaiscool@gmail.com>
2025-08-08 21:32:34 +0530

    Working code, frame extraction by time works with low accuracy, it is off actually, by some seconds, OCR added, it works.

--- extract-frame.py
+++ extract-frame.py
@@ -1,6 +1,10 @@
 import cv2
 from skimage.metrics import structural_similarity as stsi
 import numpy as np
+from PIL import Image
+import pytesseract as pt
+pt.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
 
 def load_ref_image():
     rimg = cv2.imread("r.jpg")
@@ -18,7 +22,36 @@ def extract_frames():
         compare(rimg, img)
         #cv2.imwrite("%i.jpg"%i, img)
 
+def read_frames():
+    path = "C:\\Users\\amole\\Videos\\cs.mp4"
+    v = cv2.VideoCapture(path)
+    for i in range(0, 1000):
+        s, im = v.read()
+        if i%10 == 0:
+            s = pt.image_to_string(im)
+            print(s)
+
 #load_ref_image()
+
+def extract_frame(t):
+    path = "C:\\Users\\amole\\Videos\\cs.mp4"
+    count = 5000
+    v = cv2.VideoCapture(path)
+    fr = v.get(cv2.CAP_PROP_FPS)
+    print(fr)
+    fi = int(fr * t - 1)
+
+    for i in range(0, fi - 1):
+        v.read()
+    s, i = v.read()
+    cv2.imwrite("t.png", i)
+
+def ocr():
+    i = Image.open("t.png")
+    s = pt.image_to_string(i)
+    print(s)
+
+
 
 def compare(img1, img2):
     gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -27,4 +60,7 @@ def compare(img1, img2):
     score, diff =stsi(gray1, gray2, full=True)
     print(str(score * 100))
 
-extract_frames()
+#extract_frames()
+#extract_frame(12)
+#ocr()
+read_frames()
